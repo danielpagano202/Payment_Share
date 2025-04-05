d6f97c003489424a0a35eb7323f64fa24b02c938
abim91 <53064345+abim91@users.noreply.github.com>
2025-04-05 14:44:53 -0400

    develop component to represent group ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import AddGroup from "./components/addGroup";
-export default function Home() {
+import "./page.css"
+export default function Home() {//name of the group, searching for member
   return (
     <div className={styles.main}>
       <div className="navigation">
       
+        <AddGroup name = "Bob"  />
         <AddGroup name = "Bob" />
+        <AddGroup name = "Bob" />
+        
       
         </div> 
       <div>Right Column</div>
