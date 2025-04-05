26f5685c4799a2a8815ec8b298374b041bf2a8cb
abim91 <53064345+abim91@users.noreply.github.com>
2025-04-05 16:09:42 -0400

    added modal

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,14 @@ import styles from "./page.module.css";
 import Group from "./components/group";
 import MainUser from "./components/mainUser"
 import "./page.css"
+import Modal from "./components/modal";
 export default function Home() {//name of the group, searching for member
   return (
     <div className={styles.main}>
       <div className="navigation">
+      
       <MainUser name = "Morpheus"/>
+        <Modal/>
         <Group name = "Bob"  />
         <Group name = "Bob" />
         <Group name = "Bob" />
