c78afe351b911910b90875133122474f4e5b3941
toasterstrudelz <53454635+danielpagano202@users.noreply.github.com>
2025-04-05 14:25:19 -0400

    New code

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,57 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import "./styles.css"
+class User{
+  constructor(firstName, lastName, icon){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.icon = icon;
+  }
+}
+
+class PaymentRequest{
+  constructor(user, amount, note, date){
+    this.user = user;
+    this.amount = amount;
+    this.note = note;
+    this.date = date;
+  }
+}
+
+class GroupData{
+  constructor(itemsToBePaid, name){
+    this.itemsToBePaid = itemsToBePaid;
+    this.name = name;
+  }
+}
+let exampleRequest = new PaymentRequest(
+  new User("Daniel", "Pagano", ""),
+  20,
+  "Bought Uber",
+  ""
+)
+
+let rightSideData = new GroupData(
+  [exampleRequest], "Friends"
+)
+
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <div>Left Column</div>
-      <div>Right Column</div>
+      <div style={{flexGrow: 1}}>Left Column</div>
+      <div className={styles.verticalLine}></div>
+      <div style={{flexGrow: 4}}>
+        <div className={styles.titleRow}>
+          <p>$</p>
+          <p>rightSideData.name</p>
+          <button>Settings</button>
+        </div>
+        <div class="graph">
+          
+
+        </div>
+      </div>
     </div>
   );
 }
